f688d56ad6e26a344cb3b5e5b1120081e5d5d4ab
thiarecaceresgu-a11y <thiarecaceresgu@gmail.com>
2026-01-04 18:12:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const ramos = document.querySelectorAll(".ramo");
 
-  ramos.forEach(function (ramo) {
-    ramo.addEventListener("click", function () {
+  ramos.forEach(ramo => {
+    ramo.addEventListener("click", () => {
+
+      if (ramo.classList.contains("bloqueado")) return;
+
       ramo.classList.toggle("aprobado");
+
+      ramos.forEach(r => {
+        if (!r.dataset.prereq) return;
+
+        const prereqs = r.dataset.prereq.split(",");
+        const aprobados = [...document.querySelectorAll(".ramo.aprobado")]
+          .map(x => x.dataset.id);
+
+        if (prereqs.every(p => aprobados.includes(p))) {
+          r.classList.remove("bloqueado");
+        }
+      });
     });
   });
 });
+
